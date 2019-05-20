b8a01b1222bd9863ef100bb04649574ef72e1e9b
Maxim Agrigoroae <maxim.agrigoroae@gmail.com>
2019-05-20 15:43:33 +0300

    first commit

---
+++ models/blog.model.js
@@ -0,0 +1,23 @@
+/**
+ * Example of model
+ */
+
+const mongoose = require('mongoose');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var blogSchema = new Schema({
+  title:  String,
+  author: String,
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs:  Number
+  }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
